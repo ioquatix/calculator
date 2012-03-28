c35abd4e274717acbd23632cfda8437835096abc
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-03-29 00:04:02 +1300

    Added some documentation, minor tidy up of code.

--- Calculator/CAppDelegate.h
+++ Calculator/CAppDelegate.h
@@ -8,6 +8,14 @@
 
 #import <Cocoa/Cocoa.h>
 
+// Step 1: User interface
+//	- Add text box.
+//	- Add number buttons, operator buttons, equals button and clear button.
+// Step 5: User interface
+//	- Bind text box value to app delegate value (check "Continuously Updates Value").
+//	- Connect buttons to appropriate target-action invocations below.
+
+// Step 2a: Add a set of constants for the various tags we will use for each operator button:
 typedef enum {
 	CPlus = 1,
 	CMinus = 2,
@@ -22,26 +30,25 @@ typedef enum {
 	
 	COperator _operator;
 	
+	// Step 6b: Keep a copy of the operator that was pressed:
 	NSString * _operatorString;
 }
 
 @property (assign) IBOutlet NSWindow *window;
 
-@property(nonatomic,retain) NSDecimalNumber * value;
+// Step 2c: Add properties for accessing state:
 @property(nonatomic,retain) NSString * display;
+@property(nonatomic,retain) NSDecimalNumber * value;
+@property(nonatomic,assign) COperator operator;
 
-@property(nonatomic,retain) NSString * operatorString;
+// Step 6a: Extra for expert, display current operator string:
+@property(nonatomic,retain,readonly) NSString * operatorString;
 
+// Step 4a: Add target-action invocation selectors:
 - (IBAction)pressNumber:(id)sender;
 - (IBAction)pressOperator:(id)sender;
 
 - (IBAction)pressEquals:(id)sender;
 - (IBAction)pressClear:(id)sender;
-
-@end
-
-@interface NSObject (Foobar)
-
-- foobar;
 
 @end
--- Calculator/CAppDelegate.m
+++ Calculator/CAppDelegate.m
@@ -8,42 +8,62 @@
 
 #import "CAppDelegate.h"
 
+@interface CAppDelegate ()
+@property(nonatomic,retain) NSString * operatorString;
+@end
+
 @implementation CAppDelegate
 
-@synthesize window = _window;
-@synthesize display = _display, value = _value;
-@synthesize operatorString = _operatorString;
-
+// Step 2d: Add synthesize for properties:
+@synthesize window = _window, value = _value, display = _display, operator = _operator, operatorString = _operatorString;
 
 - (void)dealloc
 {
+	self.value = nil;
+	self.display = nil;
+	self.operatorString = nil;
+	
     [super dealloc];
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-	// Insert code here to initialize your application
-	self.display = @"";
-	self.value = [NSDecimalNumber zero];
+	// Step 3: Set reasonable defaults:
+	[self pressClear:self];
 }
 
-- (void)pressClear:(id)sender
+// Step 4b: Implement number button handling:
+- (IBAction)pressNumber:(id)sender
 {
-	self.display = @"";
-	self.value = [NSDecimalNumber zero];
-	_operator = 0;
+	self.display = [NSString stringWithFormat:@"%@%d", self.display, [sender tag]];
 }
 
+// Step 4d: Implement operators button press:
+- (IBAction)pressOperator:(id)sender
+{
+	// Save the operator that has been pressed:
+	self.operator = [sender tag];
+	self.operatorString = [(NSButton*)sender title];
+	
+	if (![self.display isEqualToString:@""]) {		
+		// Copy the number being displayed into the value buffer:
+		self.value = [NSDecimalNumber decimalNumberWithString:self.display];
+		
+		self.display = @"";
+	}
+}
+
+// Step 4e: Implement equals button press:
 - (void)pressEquals:(id)sender
 {
 	NSDecimalNumber * next = [NSDecimalNumber decimalNumberWithString:self.display];
 	
-	switch (_operator) {
+	switch (self.operator) {
 		case CPlus:
 			self.value = [self.value decimalNumberByAdding:next];
 			self.display = [self.value stringValue];
 			break;
-			
+		
 		case CMinus:
 			self.value = [self.value decimalNumberBySubtracting:next];
 			self.display = [self.value stringValue];
@@ -53,7 +73,7 @@
 			self.value = [self.value decimalNumberByMultiplyingBy:next];
 			self.display = [self.value stringValue];
 			break;
-			
+		
 		case CDivide:
 			self.value = [self.value decimalNumberByDividingBy:next];
 			self.display = [self.value stringValue];
@@ -64,18 +84,13 @@
 	}
 }
 
-- (void)pressNumber:(id)sender
+// Step 4f: Implement clear button press:
+- (void)pressClear:(id)sender 
 {
-	self.display = [NSString stringWithFormat:@"%@%d", self.display, [sender tag]];
-}
-
-- (void)pressOperator:(id)sender
-{
-	_operator = [sender tag];
-	self.value = [NSDecimalNumber decimalNumberWithString:self.display];
+	self.value = nil;
 	self.display = @"";
-	
-	self.operatorString = [(NSButton*)sender title];
+	self.operator = 0;
+	self.operatorString = @"";
 }
 
 @end
