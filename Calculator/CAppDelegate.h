2bb1f806d1ee912f410b0f80da3b12ae468f4037
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-03-28 14:50:38 +1300

    First version.

--- Calculator/CAppDelegate.h
+++ Calculator/CAppDelegate.h
@@ -8,8 +8,40 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface CAppDelegate : NSObject <NSApplicationDelegate>
+typedef enum {
+	CPlus = 1,
+	CMinus = 2,
+	CMultiply = 3,
+	CDivide = 4
+} COperator;
+
+@interface CAppDelegate : NSObject <NSApplicationDelegate> {
+	// Step 2b: Add variables for calculator state:
+	NSDecimalNumber * _value;
+	NSString * _display;
+	
+	COperator _operator;
+	
+	NSString * _operatorString;
+}
 
 @property (assign) IBOutlet NSWindow *window;
+
+@property(nonatomic,retain) NSDecimalNumber * value;
+@property(nonatomic,retain) NSString * display;
+
+@property(nonatomic,retain) NSString * operatorString;
+
+- (IBAction)pressNumber:(id)sender;
+- (IBAction)pressOperator:(id)sender;
+
+- (IBAction)pressEquals:(id)sender;
+- (IBAction)pressClear:(id)sender;
+
+@end
+
+@interface NSObject (Foobar)
+
+- foobar;
 
 @end
--- Calculator/CAppDelegate.m
+++ Calculator/CAppDelegate.m
@@ -11,6 +11,9 @@
 @implementation CAppDelegate
 
 @synthesize window = _window;
+@synthesize display = _display, value = _value;
+@synthesize operatorString = _operatorString;
+
 
 - (void)dealloc
 {
@@ -20,6 +23,59 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
 	// Insert code here to initialize your application
+	self.display = @"";
+	self.value = [NSDecimalNumber zero];
+}
+
+- (void)pressClear:(id)sender
+{
+	self.display = @"";
+	self.value = [NSDecimalNumber zero];
+	_operator = 0;
+}
+
+- (void)pressEquals:(id)sender
+{
+	NSDecimalNumber * next = [NSDecimalNumber decimalNumberWithString:self.display];
+	
+	switch (_operator) {
+		case CPlus:
+			self.value = [self.value decimalNumberByAdding:next];
+			self.display = [self.value stringValue];
+			break;
+			
+		case CMinus:
+			self.value = [self.value decimalNumberBySubtracting:next];
+			self.display = [self.value stringValue];
+			break;
+			
+		case CMultiply:
+			self.value = [self.value decimalNumberByMultiplyingBy:next];
+			self.display = [self.value stringValue];
+			break;
+			
+		case CDivide:
+			self.value = [self.value decimalNumberByDividingBy:next];
+			self.display = [self.value stringValue];
+			break;
+			
+		default:
+			break;
+	}
+}
+
+- (void)pressNumber:(id)sender
+{
+	self.display = [NSString stringWithFormat:@"%@%d", self.display, [sender tag]];
+}
+
+- (void)pressOperator:(id)sender
+{
+	_operator = [sender tag];
+	self.value = [NSDecimalNumber decimalNumberWithString:self.display];
+	self.display = @"";
+	
+	self.operatorString = [(NSButton*)sender title];
 }
 
 @end
